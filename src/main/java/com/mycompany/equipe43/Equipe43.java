547a8e3da5ac2b6fbb51fa55f2298ba563f64b6f
Rana1701 <rana.azemdroub.1@ulaval.ca>
2025-10-30 16:16:25 -0400

    first commit

---
+++ src/main/java/com/mycompany/equipe43/Equipe43.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.equipe43;
+import java.awt.FlowLayout;
+
+/**
+ *
+ * @author etudiant
+ */
+public class Equipe43 {
+
+    public static void main(String[] args) {
+                /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+}
